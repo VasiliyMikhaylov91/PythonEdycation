a2993c7b4228fb89f66166d21e02a627da021cc0
VaasiliyMikhaylov91 <chelchik91@mail.ru>
2022-09-04 20:30:20 +0700

    Correct homework Lesson2

--- homeworks/Lesson2/Tasks.py
+++ homeworks/Lesson2/Tasks.py
@@ -51,37 +51,38 @@
 Найдите произведение элементов на указанных позициях. 
 Позиции хранятся в файле file.txt в одной строке одно число.
 '''
-# from random import randint
+from random import randint
 
-# element_numbers = int(input('Введите натуральное число '))
-# list = []
-# for i in range(0, element_numbers):
-#     list.append(randint(-element_numbers, element_numbers))
-# print(list)
-# f = open('file.txt', 'w', encoding='utf-8')
-# for i in range(1, randint(1, element_numbers)):
-#     f.write(str(randint(0, element_numbers - 1)) + '\n')
-# f.close()
+element_numbers = int(input('Введите натуральное число '))
+list = []
+for i in range(0, element_numbers):
+    list.append(randint(-element_numbers, element_numbers))
+print(list)
+f = open('file.txt', 'w', encoding='utf-8')
+for i in range(1, randint(1, element_numbers)):
+    f.write(str(randint(0, element_numbers - 1)) + '\n')
+f.close()
 
-# f = open('file.txt', 'r', encoding='utf-8')
-# for line in f:
-#     print(list[int(line)], end = ' ')
-# f.close()
-
+f = open('file.txt', 'r', encoding='utf-8')
+miltiplication = 1
+for line in f:
+    miltiplication *= int(line)
+f.close()
+print(miltiplication)
 '''
 5. Реализуйте алгоритм перемешивания списка.
 '''
-from random import randint
+# from random import randint
 
-list = []
-for i in range(0, randint(1, 10)):
-    list.append(randint(0, 50))
-print(list)
+# list = []
+# for i in range(0, randint(1, 10)):
+#     list.append(randint(0, 50))
+# print(list)
 
-for i in range(1, randint(10, 20)):
-    first_sort_element = randint(0, len(list) - 1)
-    second_sort_element = randint(0, len(list) - 1)
-    temp = list[first_sort_element]
-    list[first_sort_element] = list[second_sort_element]
-    list[second_sort_element] = temp
-print(list)
+# for i in range(1, randint(10, 20)):
+#     first_sort_element = randint(0, len(list) - 1)
+#     second_sort_element = randint(0, len(list) - 1)
+#     temp = list[first_sort_element]
+#     list[first_sort_element] = list[second_sort_element]
+#     list[second_sort_element] = temp
+# print(list)
