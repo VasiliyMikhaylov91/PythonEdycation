a7a82173757b0ebcb0a7b1f3715be7ba64205e78
VaasiliyMikhaylov91 <chelchik91@mail.ru>
2022-09-08 18:56:07 +0700

    Correction in task2 Lesson3

--- homeworks/Lesson3/Tasks.py
+++ homeworks/Lesson3/Tasks.py
@@ -18,7 +18,8 @@
 # print(f'Сумма элементов на нечетных позициях {summ}')
 
 '''
-2 Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+2 Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, 
+второй и предпоследний и т.д.
 
 Пример:
 
@@ -26,6 +27,7 @@
 - [2, 3, 5, 6] => [12, 15]
 '''
 # from random import randint
+# from math import ceil
 
 # start_list = []
 # for i in range (0, randint(1, 10)):
@@ -33,7 +35,7 @@
 # print(start_list, end=' => ')
 
 # end_list = []
-# for i in range(0, int(len(start_list)/2)):
+# for i in range(0, ceil(len(start_list)/2)):
 #     end_list.append(start_list[i] * start_list[len(start_list) - 1 -i])
 # print(end_list)
 
