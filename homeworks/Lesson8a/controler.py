3ff92499aa313a38b1eedc918c44f8ca83d41d16
VaasiliyMikhaylov91 <chelchik91@mail.ru>
2022-09-29 20:34:31 +0700

    Some results in lesson8a

--- homeworks/Lesson8a/controler.py
+++ homeworks/Lesson8a/controler.py
@@ -1,19 +1,17 @@
 import os
 import Data.db_process as dp, Interfase.gui as gi, functions.list_process as lp
-import time
 
 def phone_info_process():
     cont_process = True
     phone_list = dp.load_db(file_path := gi.get_path())
     while cont_process:
-        match gi.show_list(phone_list):
+        match gi.show_list(phone_list,'Список контактов', gi.option_button):
             case '1':
-                gi.result_search(lp.search(phone_list, gi.request()))
-                time.sleep(0.1)
+                gi.show_list(lp.search(phone_list,gi.request()), 'Результаты поиска', gi.result_search)
             case '2':
                 lp.new_record(phone_list, gi.new_person())
             case '3':
-                phone_list = lp.del_record(phone_list, gi.request('Кого удалить? '))
+                phone_list = lp.del_record(phone_list, gi.show_list(phone_list,'Кого удалить?', lambda: gi.del_contact(len(phone_list))))
             case '4':
                 dp.save_db(phone_list, file_path)
                 cont_process = False
--- homeworks/Lesson8a/functions/list_process.py
+++ homeworks/Lesson8a/functions/list_process.py
@@ -1,9 +1,10 @@
 def search(input_list: list, serch_str: str) -> list:
-    return [i for i in input_list if serch_str in i]
+    return list(filter(lambda x: serch_str in x, input_list))
 
 def new_record(input_list: list, added_str: str) -> list:
     input_list.append(added_str)
     return input_list
 
-def del_record(input_list: list, del_str: str) -> str:
-    return [i for i in input_list if not (del_str in i)]
+def del_record(input_list: list, del_number: int) -> str:
+    input_list.pop(del_number)
+    return input_list
