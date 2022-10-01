6bee681107632a7142a8edf9893b858ee46fd18b
VaasiliyMikhaylov91 <chelchik91@mail.ru>
2022-10-01 22:11:57 +0700

    Add Seminar8

---
+++ homeworks/some_tasks/PyQt_learning/main.py
@@ -0,0 +1,9 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import design
+
+class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
+    def __init__(self):
+        # Это здесь нужно для доступа к переменным, методам
+        # и т.д. в файле design.py
+        super().__init__()
+        self.setupUi(self)
