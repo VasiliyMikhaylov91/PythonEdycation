3936bb65d875bae29183e271b36b058e149decb8
VaasiliyMikhaylov91 <chelchik91@mail.ru>
2022-10-06 18:22:42 +0700

    qt lening

--- homeworks/some_tasks/PyQt_learning/main.py
+++ homeworks/some_tasks/PyQt_learning/main.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
 import design
 
 class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
@@ -7,3 +8,12 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         # и т.д. в файле design.py
         super().__init__()
         self.setupUi(self)
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
+    window = ExampleApp()  # Создаём объект класса ExampleApp
+    window.show()  # Показываем окно
+    app.exec_()  # и запускаем приложение
+
+if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
+    main()  # то запускаем функцию main()
