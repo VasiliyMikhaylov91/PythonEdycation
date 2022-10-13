e79d29eb6ca23efdee2695646670e70b9af745da
VasiliyMikhaylov91 <chelchik91@mail.ru>
2022-10-13 19:40:34 +0700

    Add Lesson11b

--- homeworks/Lesson10b/youbot/downloader.py
+++ homeworks/Lesson10b/youbot/downloader.py
@@ -2,7 +2,7 @@ from pytube import YouTube
 from os.path import isfile
 
 
-async def tube_download(url):
+def tube_download(url):
     video = YouTube(url)
     vid_title = video.title.replace('|','')\
                             .replace(':','')\
@@ -17,3 +17,6 @@ async def tube_download(url):
     while not isfile('homeworks\Lesson10b\youbot\ '.replace(' ','')+vid_title):
         continue
     return vid_title
+
+if __name__ == '__main__':
+    tube_download('https://www.youtube.com/watch?v=aQ9p9FVbDJo&t=249s')
